8266937743edcd390b28a9cf86d4dda66d2e8374
José María Muñoz Quijada <JoseMariaMQ@users.noreply.github.com>
2021-02-17 11:55:41 +0100

    Links

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,9 @@ import {
 function App() {
   return (
     <Router>
+      <Link to="/inicio">Inicio</Link>
+      <Link to="/">Base</Link>
+
       <Switch>
         <Route exact path="/">
           <Base/>
